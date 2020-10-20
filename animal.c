c8c501c16ea051dffd7fdad9ed44d629970fa9e3
osama <hanhan.osama@gmail.com>
2020-10-19 22:39:17 -0700

    find parents function build with weighted randomness

--- animal.c
+++ animal.c
@@ -1,5 +1,5 @@
 #define _GNU_SOURCE
-#define NUM_OF_POPULATION 10
+#define NUM_OF_POPULATION 2
 #define NUM_OF_FITNESS_INDICES 10
 
 #include <stdio.h>
@@ -8,40 +8,6 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-/*
-
-start with X threads
-
-each thread randomly fills an array with numbers
-There will be an array of best fit
-array of arrays of best 
-
-
-have compariiison functio return 0 to 1
-
-for each child, choose two parents using the wieghted random fitness
-
-5 10 15 20
-50 total
-
-5/50 = 1/10
-10/50 = 2/10
-15/50 = 3/10
-20/50 = 4/10
-
-9/30
-8/30
-7/30
-6/30
-
-
-
-for each child randomly mutate % of indices to mutate
-
-parents weight over the sum of all the weights
-
-
-*/
 typedef struct
 {
     double fitness[NUM_OF_FITNESS_INDICES];
@@ -54,10 +20,12 @@ void randomNumArr(double *arr);
 void *initializePopulation(void *person);
 void printArr(double arr[]);
 double fitnessComparison(double individual[], double goal[]);
+void *findParents(void *totalSum);
 
 double bestFit[NUM_OF_FITNESS_INDICES];
-individual *population[NUM_OF_POPULATION]; //big number, used to filter to small population
-int populationIndex = 0;
+individual *population[NUM_OF_POPULATION];
+
+individual *parents[NUM_OF_POPULATION * 2];
 
 pthread_mutex_t mutex; //mutex used for writing to shared childPopulation array
 
@@ -75,8 +43,6 @@ int main()
     }
 
     double totalSum = 0;
-    double weights[NUM_OF_POPULATION];
-
     //joins all the threads
     for (int i = 0; i < NUM_OF_POPULATION; i++)
     {
@@ -84,18 +50,56 @@ int main()
         totalSum += population[i]->fitnessIndex;
     }
 
+    //threads to find parents
     for (int i = 0; i < NUM_OF_POPULATION; i++)
     {
-        weights[i] = ((1 - ((population[i]->fitnessIndex) / totalSum)) / (NUM_OF_POPULATION - 1)); //gives you each weight which added together give 1.0
+        pthread_create(&threadNums[i], NULL, findParents, (void *)&totalSum); //i send totalSum because i dont want to loop and calculate it each time
     }
 
-    struct timeval time;
-    gettimeofday(&time, NULL);
+    //joins all the threads
+    for (int i = 0; i < NUM_OF_POPULATION; i++)
+    {
+        pthread_join(threadNums[i], NULL);
+    }
 
-    int t = time.tv_usec; //random math to maybe make it more chaotic?
+    for (int i = 0; i < NUM_OF_POPULATION * 2; i++)
+        printf("%f\n", parents[i]->fitnessIndex);
+}
 
-    double newParent = ((double)(rand_r(&t) % 100)) / 100;
-    printf("%f\n", newParent);
+void *findParents(void *totalSum)
+{
+
+    double weights[NUM_OF_POPULATION];
+
+    double sum = *((double *)totalSum);
+
+    for (int i = 0; i < NUM_OF_POPULATION; i++)
+    {
+
+        weights[i] = ((1 - ((population[i]->fitnessIndex) / sum)) / (NUM_OF_POPULATION - 1)); //gives you each weight which added together give 1.0
+    }
+
+    for (int i = 0; i < NUM_OF_POPULATION * 2; i++)
+    {
+
+        struct timeval time;
+        gettimeofday(&time, NULL);
+
+        int t = time.tv_usec; //random math to maybe make it more chaotic?
+
+        double newParent = ((double)(rand_r(&t) % 100)) / 100;
+
+        double num = 0;
+        for (int j = 0; j < NUM_OF_POPULATION; j++)
+        {
+            num += weights[j];
+            if (newParent < num)
+            {
+                parents[i] = population[j];
+                j = NUM_OF_POPULATION;
+            }
+        }
+    }
 }
 
 /*
