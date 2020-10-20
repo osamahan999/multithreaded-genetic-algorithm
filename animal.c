e52d71d41137d07f61683290cdb24b906aa0b6a5
osama <hanhan.osama@gmail.com>
2020-10-20 12:39:22 -0700

    fixed multithreading of parents and seg fault issue

--- animal.c
+++ animal.c
@@ -1,7 +1,7 @@
 #define _GNU_SOURCE
-#define NUM_OF_POPULATION 20
+#define NUM_OF_POPULATION 100
 #define NUM_OF_FITNESS_INDICES 10
-#define NUM_OF_GENS 2
+#define NUM_OF_GENS 100
 #define NUM_OF_PARENTS (2 * NUM_OF_POPULATION) //amt of parents each child has
 
 #include <stdio.h>
@@ -24,7 +24,7 @@ void randomNumArr(double *arr);
 void *initializePopulation(void *person);
 void printArr(double arr[]);
 double fitnessComparison(double individual[], double goal[]);
-void *findParents();
+void *findParents(void *i);
 void parentThreadingFunc();
 void initializePopulationThreading();
 void haveChildren();
@@ -33,15 +33,14 @@ void calculatePopulationWeights();
 
 double bestFit[NUM_OF_FITNESS_INDICES];
 individual *population[NUM_OF_POPULATION];
+individual *initialPopulation[NUM_OF_POPULATION]; //duplicates population array to free the pointers
 
 individual *parents[NUM_OF_POPULATION * 2];
-int parentIndex = 0;
 sem_t parentUsed; //used to control writing to parents array
 
 int main()
 {
-    randomNumArr(bestFit);       //generates what we will be regarding the best fit array
-    sem_init(&parentUsed, 0, 1); //initialized to 1 because I want to alternate between 1 and 0 for the lock.
+    randomNumArr(bestFit); //generates what we will be regarding the best fit array
 
     initializePopulationThreading();
     printf("gen #%d %f\n", 0, getError());
@@ -57,7 +56,7 @@ int main()
 
     //dealloc
     for (int i = 0; i < NUM_OF_POPULATION; i++)
-        free(population[i]);
+        free(initialPopulation[i]);
 }
 
 //get total absolute val error on avg just for debugging
@@ -113,7 +112,9 @@ void initializePopulationThreading()
 
     for (int i = 0; i < NUM_OF_POPULATION; i++)
     {
-        population[i] = (individual *)malloc(sizeof(individual)); //allocate mem for each population
+        //allocate mem for each population, store original points in initialPopulation array to free them at end of run
+        initialPopulation[i] = (individual *)malloc(sizeof(individual));
+        population[i] = initialPopulation[i];
 
         pthread_create(&threadNums[i], NULL, initializePopulation, (void *)population[i]);
     }
@@ -141,10 +142,14 @@ void parentThreadingFunc()
     pthread_t threadNums[NUM_OF_PARENTS];
     calculatePopulationWeights();
 
-    //threads to find parents
-    for (int i = 0; i < NUM_OF_PARENTS; i++)
+    //threads to find
+
+    for (int parent = 0; parent < NUM_OF_PARENTS; parent++)
     {
-        pthread_create(&threadNums[i], NULL, findParents, NULL); //each parent found using x threads
+        int *j = (int *)malloc(sizeof(int)); //mallocs int such that value does change due to synchronization issues
+
+        *j = parent;                                                       //sets value of malloc'd int to the parent index
+        pthread_create(&threadNums[parent], NULL, findParents, (void *)j); //each parent found using x threads
     }
 
     //joins all the threads
@@ -159,11 +164,13 @@ Finds the new parents for the next children and puts them in the parents array
 Uses weighted randomness, where each individual is given a weight due to their relative fitness, and then uses a random number to choose
 one of the weighted parents and puts it in the array
 */
-void *findParents()
+void *findParents(void *pointerToParentIndex)
 {
 
     struct timeval time;
     double num;
+    int parentIndex = *(int *)pointerToParentIndex;
+    free(pointerToParentIndex);
 
     num = 0; //sum of weights to find which parent to use
 
@@ -172,7 +179,6 @@ void *findParents()
 
     double newParent = ((double)(rand_r(&t) % 100000)) / 100000; //0 to 999999 because our random doubles go to 0 to 100,000
 
-    sem_wait(&parentUsed);
     for (int j = 0; j < NUM_OF_POPULATION; j++)
     {
         num += population[j]->weight;
@@ -181,11 +187,9 @@ void *findParents()
         {
             parents[parentIndex] = population[j];
 
-            parentIndex++;
             break;
         }
     }
-    sem_post(&parentUsed);
 
     pthread_exit(0); //close thread ; job done
 }
@@ -209,7 +213,7 @@ double fitnessComparison(double individual[], double goal[])
 {
     double fitnessFactor = 0;
 
-    for (int i = 0; i < NUM_OF_POPULATION; i++)
+    for (int i = 0; i < NUM_OF_FITNESS_INDICES; i++)
     {
         fitnessFactor += (fabs(individual[i] - goal[i]));
     }
